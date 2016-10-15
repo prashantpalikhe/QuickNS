6baaaedecf91a05921b6399d582b467d88a09f60
Prashant Palikhe <prashpal09@gmail.com>
2016-10-15 22:24:01 +0200

    Revert "Favorites implemented"
    This reverts commit 449b4842c6b1a015c7d80200ba289f563c22e0b1.

--- www/js/app.js
+++ www/js/app.js
@@ -30,14 +30,6 @@ angular
                 templateUrl: 'templates/departures.html',
                 controller: 'DeparturesController',
                 controllerAs: '$ctrl'
-            })
-
-            .state('bookmarks', {
-                url: '/bookmarks',
-                templateUrl: 'templates/bookmarks.html',
-                controller: 'BookmarksController',
-                controllerAs: '$ctrl',
-                cache: false
             });
 
         $urlRouterProvider.otherwise('/');
