faef99389eb21703c1284f0744ad4fd09980bec4
Prashant Palikhe <prashpal09@gmail.com>
2016-10-17 21:15:56 +0200

    Removed unused util function

--- www/js/services/util.js
+++ www/js/services/util.js
@@ -7,8 +7,7 @@
 
     function utilFactory($q, $ionicPlatform, $cordovaGeolocation) {
         return {
-            getCurrentLocation: getCurrentLocation,
-            getDistanceBetweenPoints: getDistanceBetweenPoints
+            getCurrentLocation: getCurrentLocation
         };
 
         function getCurrentLocation() {
@@ -24,24 +23,6 @@
                          });
                  });
             });
-        }
-
-        function getDistanceBetweenPoints(lat1, lon1, lat2, lon2) {
-            var R = 6371; // Radius of the earth in km
-            var dLat = deg2rad(lat2 - lat1);  // deg2rad below
-            var dLon = deg2rad(lon2 - lon1);
-            var a =
-                    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-                    Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
-                    Math.sin(dLon / 2) * Math.sin(dLon / 2)
-                ;
-            var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-            var d = R * c; // Distance in km
-            return d.toFixed(2);
-        }
-
-        function deg2rad(deg) {
-            return deg * (Math.PI/180)
         }
     }
 })();
