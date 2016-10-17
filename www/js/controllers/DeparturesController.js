93bfb10feca21e1242a17382233bd802018308cc
Prashant Palikhe <prashpal09@gmail.com>
2016-10-17 21:35:34 +0200

    Revert "Removed show departures from here"
    This reverts commit fe6909733b5834da4e59143a9f25235a7e52c153.

--- www/js/controllers/DeparturesController.js
+++ www/js/controllers/DeparturesController.js
@@ -5,7 +5,7 @@
         .module('quikns')
         .controller('DeparturesController', DeparturesController);
 
-    function DeparturesController($scope, $stateParams, $ionicActionSheet, $cordovaSocialSharing, data) {
+    function DeparturesController($scope, $state, $stateParams, $ionicActionSheet, $cordovaSocialSharing, data) {
         var ctrl = this;
         var viewEntered = false;
 
@@ -70,6 +70,7 @@
 
             var hideSheet = $ionicActionSheet.show({
                 buttons: [
+                    {text: 'Toon vertrek vanaf hier'},
                     {text: 'Deel'}
                 ],
                 titleText: departure.destination,
@@ -79,6 +80,9 @@
                 },
                 buttonClicked: function(index) {
                     if (index === 0) {
+                        $state.go('departures', {station: departure.destination});
+
+                    } else if (index === 1) {
                         share(departure);
                     }
                 }
